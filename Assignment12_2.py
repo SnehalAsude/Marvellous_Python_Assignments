fd3d6c734adf318498c9d2e000e7ba02c2388c1c
SnehalAsude <snehaasude@gmail.com>
2025-06-04 17:11:08 +0530

    Add files via upload

---
+++ Assignment12_2.py
@@ -0,0 +1,36 @@
+
+class Circle:
+    PI = 3.14
+
+    def __init__(self):
+        self.radius = 0.0
+        self.area = 0.0
+        self.circumference = 0.0
+
+
+    def Accept(self,val1):
+        self.radius = val1
+
+    def calculateArea(self):
+        self.area = Circle.PI * self.radius * self.radius
+
+    def calculateCircumference(self):
+        self.circumference =2 * Circle.PI  * self.radius    
+
+    def Display(self):
+        print("Area of circle is:",self.area)
+        print("circumference of circle is:",self.circumference)
+        
+
+def main():
+    obj1 = Circle()
+    print("enter radius")
+    no = int(input())
+    obj1.Accept(no)
+    obj1.calculateArea()
+    obj1.calculateCircumference()
+    obj1.Display()
+
+
+if __name__ == "__main__":
+    main()    
